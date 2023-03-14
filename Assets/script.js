7e0e61d8500eb9fd8fe9692faa18c35495f3f59e
Corey Boughton <corzah911@gmail.com>
2023-03-14 19:30:29 +1100

    adding the time functions, if, else if and else statements

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,54 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+// This var is to display today's day and date
+var todayDate = moment().format('dddd, MMM, YYYY');
+$("currentday").html(todayDate);
+
+$(document).ready(function () {
+
+  $("saveBtn").on("click", function () {
+
+    var text = $(this).siblings(".description").val();
+    var text = $(this).parent().attr("id");
+
+    localStorage.setItem(time,text);
+  })
+})
+
+function timeTracker() {
+
+  var timeNow = moment().hour();
+
+  // Looping over time blocks
+  $(".time-block").each(function () {
+    var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+    // To check time and add the background classes for indicators
+
+   if (blockTime < timeNow){
+      $(this).removeClass("future");
+      $(this).removeClass("present");
+      $(this).addClass("past");
+   }
+
+   else if (blockTime === timeNow) {
+      $(this).removeClass("past");
+      $(this).removeClass("future");
+      $(this).addClass("present");
+   }
+
+   else {
+      $(this).removeClass("present");
+      $(this).removeClass("past");
+      $(this).addClass("future");
+   }
+
+  })
+
+}
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
