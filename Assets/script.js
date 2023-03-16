4a00513303317fc78ad2595d690d06b969ebb449
Corey Boughton <corzah911@gmail.com>
2023-03-16 19:04:21 +1100

    added a readME file with description and detail about my steps through this challenge. Also removed all the teachers comments

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,9 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
 $(document).ready(function () {
+  
   // This var is to display today's day and date
   var todayDate = dayjs().format('DD/MM/YYYY')
   $("#currentDay").html(todayDate);
@@ -9,6 +11,8 @@ $(document).ready(function () {
 
 
   $(".saveBtn").on("click", function () {
+   
+    // Get nearby values of the description in JQuery
     var saveTasks = JSON.parse(window.localStorage.getItem("saveTasks")) || []
     var text = $(this).siblings(".description").val();
     var time = $(this).parent().attr("id");
@@ -23,14 +27,17 @@ $(document).ready(function () {
 
 
   function timeTracker() {
-
+   
+    //This var is to display the correct time and day using day.js
     var timeNow = dayjs().hour();
     console.log(typeof timeNow)
+    
     // Looping over time blocks
     $(".time-block").each(function () {
       var blockTime = parseInt($(this).attr("id").split("hour-")[1]);
      console.log(typeof blockTime)
-      // To check time and add the background classes for indicators
+      
+     // To check time and add the background classes for indicators
 
       if (blockTime > timeNow) {
         $(this).addClass("future");
@@ -51,6 +58,7 @@ $(document).ready(function () {
     })
 
   }
+  
   $(".description").each(function( index, value ) {
     var id = $(this).parent().attr("id");
     for (let i = 0; i < save.length; i++) {
@@ -61,20 +69,6 @@ $(document).ready(function () {
       
     }
   });
-  // $("#hour8 .description").val(localStorage.getItem("hour8"));
-  // $("#hour9 .description").val(localStorage.getItem("hour9"));
-  // $("#hour10 .description").val(localStorage.getItem("hour10"));
-  // $("#hour11 .description").val(localStorage.getItem("hour11"));
-  // $("#hour12 .description").val(localStorage.getItem("hour12"));
-  // $("#hour13 .description").val(localStorage.getItem("hour13"));
-  // $("#hour14 .description").val(localStorage.getItem("hour14"));
-  // $("#hour15 .description").val(localStorage.getItem("hour15"));
-  // $("#hour16 .description").val(localStorage.getItem("hour16"));
-  // $("#hour17 .description").val(localStorage.getItem("hour17"));
-  // $("#hour18 .description").val(localStorage.getItem("hour18"));
-  // $("#hour19 .description").val(localStorage.getItem("hour19"));
-  // $("#hour20 .description").val(localStorage.getItem("hour20"));
-
 
   timeTracker();
 })
--- README.md
+++ README.md
@@ -1,1 +1,56 @@
 # Homework-16-3-23
+
+# 05 Challenge: Work Day Scheduler
+
+## Job Description
+
+For this weeks challenge i was given some starter code to code and create a working Work Day Scheduler using HTML, CSS, Javascript, Bootstrap and JQuery.
+
+## User Story
+
+```
+AS AN employee with a busy schedule
+I WANT to add important events to a daily planner
+SO THAT I can manage my time effectively
+```
+
+## Acceptance Criteria
+
+```
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+```
+
+## Languages Used
+HTML
+CSS
+Bootstrap
+jQuery
+Day.js
+
+## Work Done
+
+When the planner is openned, the current day is displayed at the top of the calendar
+
+When scrolled down, the user is presented with time blocks for standard business hours (from 8AM to 8PM)
+
+Calender as broken up in to 3 color-coded sections to indicate whether it is in the past present or future. -- 
+
+RED - PRESENT - ![#f03c15](https://via.placeholder.com/15/f03c15/f03c15.png) `#f03c15`
+GREEN - FUTURE - ![#c5f015](https://via.placeholder.com/15/c5f015/c5f015.png) `#c5f015`
+GREY - PAST - ![#1589F0](https://via.placeholder.com/15/808080/808080.png) `808080`
+
+When clicked on the save button on the right for that time block, then the text for that event is saved in local storage
+
+When the page is refreshed, the saved events persist
